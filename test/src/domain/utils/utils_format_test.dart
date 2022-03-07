886cdc43b00c43ad6d8f3bbabbfe54f3c7ad7496
rdelonzek <renidelonzek@gmail.com>
2022-03-07 17:53:35 -0300

    feat: test utils_format

---
+++ test/src/domain/utils/utils_format_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:select_any/select_any.dart';
+
+void main() {
+  test('Test UtilsFormat', () {
+    // Deixar com o caractere invisível, pois esse é o retorno
+    expect(UtilsFormat.formatMoney(20), 'R\$ 20,00');
+    expect(UtilsFormat.formatMoney(-42), '-R\$ 42,00');
+    expect(UtilsFormat.formatMoney(52.4), 'R\$ 52,40');
+    expect(
+        UtilsFormat.formatMoney(344.4344, maxDecimalDigits: 2), 'R\$ 344,43');
+    expect(
+        UtilsFormat.formatMoney(344.4344, maxDecimalDigits: 4), 'R\$ 344,4344');
+  });
+}

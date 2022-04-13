1a401b742bca64892dc5235aa8379b2e8f19d392
rdelonzek <renidelonzek@gmail.com>
2022-04-13 17:29:57 -0300

    feat: export font_data_filter

--- lib/src/data/repository/repository.dart
+++ lib/src/data/repository/repository.dart
@@ -1,6 +1,7 @@
+export 'action_select_impl.dart';
 export 'datasource_any.dart';
+export 'filter_base_impl.dart';
+export 'filter_exp_impl.dart';
+export 'font_data_filter_any.dart';
 export 'format_data_impl.dart';
 export 'type_data_impl.dart';
-export 'filter_exp_impl.dart';
-export 'action_select_impl.dart';
-export 'filter_base_impl.dart';
